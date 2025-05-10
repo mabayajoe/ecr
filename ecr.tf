7d99da4887f74d768bdcc8e3fa0a301642fc1133
JoeM. <75545397+mabayajoe@users.noreply.github.com>
2025-05-09 21:04:33 -0600

    ecr push

---
+++ ecr.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1" # Fixed region name with quotes and correct spelling
+}
+
+resource "aws_ecr_repository" "FirstECR" {
+  name                 = "ecr-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+}
